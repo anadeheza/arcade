c8457db12ddb0942cb3a8368bc7d39f26950b57e
Ana <adehezaflaherty@gmail.com>
2026-04-20 17:19:54 -0300

    arreglo de space-jump

--- projects/space-jump/jump-game.js
+++ projects/space-jump/jump-game.js
@@ -15,7 +15,7 @@ const platBrokenImg = new Image();
 platBrokenImg.src = 'assets/platBroken.png';
 
 let score = 0;
-let gravity = 0.6;
+let gravity = 0.4;
 let cameraY = 0;
 let shakeTime = 0;
 let shakeIntensity = 0;
